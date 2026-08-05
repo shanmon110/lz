e38d50915f5e754aca9545de50a49c6e7f778454
cuhealthybrains <lizheqlu@163.com>
2026-08-06 05:38:25 +0800

    feat: add fail-open visitor logging proxy

--- cloudflare/visitor-logging/src/index.ts
+++ cloudflare/visitor-logging/src/index.ts
@@ -1,7 +1,13 @@
 import type { Env } from "./env";
+import { handlePublicRequest } from "./public-handler";
 
 export default {
-  fetch(): Response {
+  fetch(request, env, ctx): Response | Promise<Response> {
+    const host = new URL(request.url).hostname;
+    if (host === "lizhe.link" || host === "www.lizhe.link") {
+      return handlePublicRequest(request, env, ctx);
+    }
+
     return new Response("Not Found", { status: 404 });
   }
 } satisfies ExportedHandler<Env>;
